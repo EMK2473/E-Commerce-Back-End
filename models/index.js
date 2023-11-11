34e74e5859be34c93c8f8e0f90f079769f9566d0
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-11 12:47:39 -0500

    updated notes

--- models/index.js
+++ models/index.js
@@ -1,28 +1,21 @@
 // import model paths
-const ProductModel = require('./productModel');
-const CategoryModel = require('./categoryModel');
-const TagModel = require('./tagModel');
-const ProductTagModel = require('./productTagModel');
+const ProductModel = require("./productModel");
+const CategoryModel = require("./categoryModel");
+const TagModel = require("./tagModel");
+const ProductTagModel = require("./productTagModel");
 // models are the models for each column the data tables
 // each column is defined by their properties keys&&values
 
-
-
-
-
 // relate the models here
 // using belongs and has methods
-// belongsTo 
+// belongsTo
 // belongsToMany
 // and hasmany
 // export the models here as object
 
-
-
-
 module.exports = {
-    ProductModel,
-    CategoryModel,
-    TagModel,
-    ProductTagModel,
-  };
+  ProductModel,
+  CategoryModel,
+  TagModel,
+  ProductTagModel,
+};
