93be1867f13daa08559ab5a7e9269d2ae7d09336
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-14 17:46:52 -0500

    dev comments for next session

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 // req router for product routes
 // req product model in
+// req in all models to consolidate prodTagroutes and prodRoutes into one
 const router = require("express").Router();
 const { ProductModel, CategoryModel, TagModel, ProductTagModel } = require("../models");
 
 
 
-//get
+// get
 router.get('/', async (req, res)=>{
     try{
         const products = await ProductModel.findAll({ include: [{ model: CategoryModel }, { model: TagModel }]});
@@ -24,7 +25,7 @@ router.get('/:id', async (req, res)=>{
     }
 })
 
-//post
+// post
 router.post("/", async (req, res) => {
     try {
       const product = await ProductModel.create(req.body);
@@ -42,8 +43,8 @@ router.post("/", async (req, res) => {
   });
 
 // put
-router.put('/:id', async (req, res) =>{
-    try{
+// router.put('/:id', async (req, res) =>{
+//     try{
         // if req.body.tags exists && if req.body.tags.length > 0 then 
         // await productTagModel to find all where product_id: contains req.params.id
         // then map productTagIds to be tag_id => tag_id
@@ -59,8 +60,8 @@ router.put('/:id', async (req, res) =>{
     //     ProductTag.destroy({ where: { id: productTagsToRemove } }),
     //     ProductTag.bulkCreate(newProductTags),
     //   ]);
-    }
-})
+//     }
+// })
 
 // delete
 router.delete('/:id', async (req, res) =>{
